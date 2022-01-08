5073a296a31809662075ab7c990203380cf6451b
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-01-08 12:53:25 -0800

    responsive user home page images

--- frontend/src/components/ImageDetail/index.js
+++ frontend/src/components/ImageDetail/index.js
@@ -1,13 +1,16 @@
 import { NavLink } from 'react-router-dom';
+import './ImageDetail.css';
 
 const ImageDetail = ({ image }) => {
     return (
+        // <div className='image-container'>
         <>
-        <NavLink to={`/artists/${image.userId}`}>
-            <img className='grid-image' id={image.id} key={image.id} src={image.imageUrl} alt='hello'></img>
-        </NavLink>
-        <p>Favourites: {image.favoritedCount}</p>
+            <NavLink to={`/artists/${image.userId}`}>
+                <img className='grid-image' id={image.id} key={image.id} src={image.imageUrl} alt='hello'></img>
+            </NavLink>
+            <p className='favorites-count'>Favourites: {image.favoritedCount}</p>
         </>
+        // </div>
     )
 }
 
