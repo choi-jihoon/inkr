6d80a6926b2406799568d0c0e2ae068e3c389531
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-01-09 12:07:40 -0800

    fixed portfolio zoom and edited portfolio view

--- frontend/src/components/MyPortfolio/index.js
+++ frontend/src/components/MyPortfolio/index.js
@@ -8,6 +8,8 @@ import { getArtistImages } from '../../store/artist';
 import EditPostModal from '../EditPostModal';
 import DeletePostModal from '../DeletePostModal';
 import ImageZoom from '../ImageZoom';
+// import ImageDetail from '../ImageDetail';
+import MyPortfolioImageDetail from '../MyPortfolioImageDetail';
 
 import './MyPortfolio.css';
 
@@ -34,28 +36,17 @@ const MyPortfolio = () => {
         <div className='main-container'>
             <h2>My Portfolio</h2>
             <div className='all-images-container'>
-                    {artistImages.map((image) => {
-                        let tagString;
-                        if (image.tags) {
-                            tagString = image.tags;
-                            tagString = tagString.map((tag) => `#${tag}`)
-                            tagString = tagString.join(', ')
-                        }
-                        return (
-                            <div className='image-container' key={image.id}>
-                                <img onClick={() => setShowModal(true)} className='grid-image' id={image.id} key={image.id} src={image.imageUrl} alt='hello'></img>
-                                {showModal && (
-                                    <Modal onClose={() => setShowModal(false)}>
-                                        <ImageZoom image={image} />
-                                    </Modal>
-                                )}
-                                <p className='favorites-count'>Favourites: {image.favoritedCount}</p>
-                                <p key={image}>{tagString}</p>
-                                <EditPostModal image={image} id={image.id} />
-                                <DeletePostModal image={image} id={image.id} />
-                            </div>
-                        )
-                    })}
+                {artistImages.map((image) => {
+                    let tagString;
+                    if (image.tags) {
+                        tagString = image.tags;
+                        tagString = tagString.map((tag) => `#${tag}`)
+                        tagString = tagString.join(', ')
+                    }
+                    return (
+                        <MyPortfolioImageDetail key={image.id} image={image} tagString={tagString} />
+                    )
+                })}
             </div>
         </div>
     );
---
+++ frontend/src/components/MyPortfolioImageDetail/index.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+import EditPostModal from '../EditPostModal';
+import DeletePostModal from '../DeletePostModal';
+
+import { Modal } from '../../context/Modal';
+import ImageZoom from '../ImageZoom';
+import './MyPortfolioImageDetail.css';
+
+const MyPortfolioImageDetail = ({ image, tagString }) => {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+        <div className='image-container portfolio-image' key={`${image.id}-artist-image`}>
+            <img onClick={() => setShowModal(true)} className='grid-image portfolio-grid-image' id={image.id} key={image.id} src={image.imageUrl} alt='hello'></img>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <ImageZoom image={image} tagString={tagString} key={`${image.id}-zoom`} />
+                </Modal>
+            )}
+            <div className='edit-delete-butt-container'>
+                <EditPostModal image={image} id={image.id} />
+                <DeletePostModal image={image} id={image.id} />
+            </div>
+            <div className='portfolio-image-info'>
+                <div className='tag-container'>
+                    <div className='portfolio-tags tag-text' key={image}>Tags: {tagString ? tagString.split(',').map(tag => <p key={tag} className='single-tag portfolio-tag tag-text'>{`${tag}`}</p>) : <p key='notag' className='no-tags tag-text'>No tags available</p>}</div>
+                </div>
+                <div className='favorites-count portfolio-fave-count'><i className="far fa-star portfolio-star"></i><p>{image.favoritedCount}</p></div>
+            </div>
+        </div>
+    )
+}
+
+export default MyPortfolioImageDetail;
