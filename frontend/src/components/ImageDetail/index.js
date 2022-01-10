cf9d263d2a0117839a94461f5773e796c92c3b5c
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-01-09 19:59:58 -0800

    animation for favorite star

--- frontend/src/components/ImageDetail/index.js
+++ frontend/src/components/ImageDetail/index.js
@@ -2,16 +2,16 @@ import { NavLink } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+
 import { Modal } from '../../context/Modal';
 import { addToFavorites, updateFavoriteCount } from '../../store/images';
 import ImageZoom from '../ImageZoom';
+import FavoriteStar from '../FavoriteStar';
 import './ImageDetail.css';
 
 const ImageDetail = ({ image, tagString }) => {
-    const dispatch = useDispatch();
     const [showModal, setShowModal] = useState(false);
-    const [count, setCount] = useState(image.favoritedCount);
-    const sessionUser = useSelector(state => state.session.user);
+
 
     // useEffect(() => {
     //     const modal = document.querySelector('#modal-content');
@@ -22,30 +22,6 @@ const ImageDetail = ({ image, tagString }) => {
 
     // }, [showModal])
 
-    const handleFavorite = async (e) => {
-        e.preventDefault();
-
-        setCount(prevState => prevState + 1);
-
-        const payload = {
-            userId: sessionUser.id,
-            imageId: image.id
-        }
-
-        await dispatch(addToFavorites(payload));
-
-        console.log(count)
-
-        const payload2 = {
-            id: image.id,
-            userId: image.userId,
-            imageUrl: image.imageUrl,
-            tags: image.tags,
-            favoritedCount: (count + 1)
-        }
-
-        await dispatch(updateFavoriteCount(payload2));
-    }
 
 
     return (
@@ -61,15 +37,7 @@ const ImageDetail = ({ image, tagString }) => {
                     <p className='artist-name'>{image?.User.username}</p>
                 </NavLink>
                 <div className='favorites-count'>
-                    <button
-                        className='favorited-button'
-                        onClick={handleFavorite}
-                    >
-                        <i className="far fa-star"></i>
-                    </button>
-                    <p className='favorites-number'>
-                        {count}
-                    </p>
+                    <FavoriteStar image={image} tagString={tagString} />
                 </div>
             </div>
         </div>
