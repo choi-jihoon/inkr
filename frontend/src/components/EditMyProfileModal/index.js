be7e5f283fef5e96c66179a4d1c3e11df89b5578
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-01-11 11:30:11 -0800

    edit profile and fixed store for create

---
+++ frontend/src/components/EditMyProfileModal/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Modal } from '../../context/Modal';
+import EditMyProfileForm from './EditMyProfileForm.js';
+
+function EditMyProfileModal({ myProfile }) {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button className='edit-my-profile-button' onClick={() => setShowModal(true)}><i className="fas fa-edit edit-profile"></i></button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <EditMyProfileForm showModal={setShowModal} myProfile={myProfile} />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default EditMyProfileModal;
--- frontend/src/components/MyProfile/index.js
+++ frontend/src/components/MyProfile/index.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { getMyProfile } from '../../store/profiles';
+import EditMyProfileModal from '../EditMyProfileModal';
 
 import './MyProfile.css';
 
@@ -32,23 +33,28 @@ const MyProfile = () => {
                         <img className='my-profile-image' src={myProfile?.profilePic} alt={`${myProfile?.User.username}'s profile`}></img>
                     </div>
                 </div>
-                <div className='my-profile'>
-                    <div className='my-profile-fullName'>
-                        <h4>
-                            {myProfile?.fullName}
-                        </h4>
-                    </div>
-                    <div className='my-profile-username'>
-                        {myProfile?.User.username}
-                    </div>
-                    <div className='my-profile-location'>
-                        Location: {myProfile?.location}
-                    </div>
-                    <div className='my-profile-specialties'>
-                        Specialties: {myProfile?.specialties}
-                    </div>
-                    <div className='my-profile-description'>
-                        About the artist: {myProfile?.description}
+                <EditMyProfileModal myProfile={myProfile} />
+                <div className='my-profile-content-container'>
+                    <div className='my-profile'>
+                        <div className='my-profile-fullName'>
+                            <h4>
+                                {myProfile?.fullName}
+                            </h4>
+                        </div>
+                        <div className='my-profile-username'>
+                            {myProfile?.User.username}
+                        </div>
+                        <div className='my-profile-location'>
+                            <p className='my-profile-label'>Location: <span className='profile-location-text'>{myProfile?.location}</span></p>
+                        </div>
+                        <div className='my-profile-specialties'>
+                            <p className='my-profile-label'>Specialties</p>
+                            <p>{myProfile?.specialties.join(', ')}</p>
+                        </div>
+                        <p className='my-profile-label'>About the artist</p>
+                        <div className='my-profile-description'>
+                            <p className='my-profile-description-text'>{myProfile?.description}</p>
+                        </div>
                     </div>
                 </div>
 
