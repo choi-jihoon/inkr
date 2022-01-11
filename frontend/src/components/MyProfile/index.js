64f3ffa0fea2864b9c02c8758ca0631b918309c6
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-01-10 20:34:46 -0800

    my profile styling

--- frontend/src/components/MyProfile/index.js
+++ frontend/src/components/MyProfile/index.js
@@ -11,13 +11,12 @@ const MyProfile = () => {
     const dispatch = useDispatch();
 
     const sessionUser = useSelector((state) => state.session.user);
+    const sessionProfile = useSelector(state => state.profile);
+    const myProfile = sessionProfile[0];
 
     if (!sessionUser) {
         history.push('/');
     }
-
-    const sessionProfile = useSelector(state => state.profile);
-    const myProfile = sessionProfile[0];
 
     useEffect(() => {
         dispatch(getMyProfile(sessionUser.id))
@@ -25,9 +24,34 @@ const MyProfile = () => {
 
     return (
         <div className='main-container my-profile-container'>
-            <h2 className='my-profile-header'>Profile Settings</h2>
-            <div className='my-profile'>
-                {myProfile.fullName}
+            <h2 className='my-profile-header'>My Profile</h2>
+            <div className='my-profile-content'>
+
+                <div className='my-profile-image-div-container'>
+                    <div className='my-profile-image-div'>
+                        <img className='my-profile-image' src={myProfile?.profilePic} alt={`${myProfile?.User.username}'s profile`}></img>
+                    </div>
+                </div>
+                <div className='my-profile'>
+                    <div className='my-profile-fullName'>
+                        <h4>
+                            {myProfile?.fullName}
+                        </h4>
+                    </div>
+                    <div className='my-profile-username'>
+                        {myProfile?.User.username}
+                    </div>
+                    <div className='my-profile-location'>
+                        Location: {myProfile?.location}
+                    </div>
+                    <div className='my-profile-specialties'>
+                        Specialties: {myProfile?.specialties}
+                    </div>
+                    <div className='my-profile-description'>
+                        About the artist: {myProfile?.description}
+                    </div>
+                </div>
+
             </div>
         </div>
     )
