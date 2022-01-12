6c7b0e9adf0520f97bd446d2bd1830405c939a1b
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-01-12 15:23:41 -0800

    more styling

--- frontend/src/components/ReviewDetail/index.js
+++ frontend/src/components/ReviewDetail/index.js
@@ -8,23 +8,68 @@ const ReviewDetail = ({ review }) => {
 
     const date = new Date(review.createdAt).toDateString();
 
-    return (
-        <div className='review-container'>
-            <div className='reviewed-by'>
-                {review.User.username}
+    let ratingStars;
+    if (Math.floor(review.rating) === 1) {
+        ratingStars = (
+            <>
+                <span><i className="fas fa-star review-star"></i></span>
+            </>
+        )
+    } else if (Math.floor(review.rating) === 2) {
+        ratingStars = (
+            <>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+            </>
+        )
+    } else if (Math.floor(review.rating) === 3) {
+        ratingStars = (
+            <>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+            </>
+        )
+    } else if (Math.floor(review.rating) === 4) {
+        ratingStars = (
+            <>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+            </>
+        )
+    } else {
+        ratingStars = (
+            <>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+                <span><i className="fas fa-star review-star"></i></span>
+            </>
+        )
+    }
+
+        return (
+            <div className='review-container'>
+                <div className='reviewed-by-and-rating'>
+                    <div className='reviewed-by'>
+                        {review.User.username}
+                    </div>
+                </div>
+                    <div className='review-rating'>
+                        {ratingStars}
+                    </div>
+                <div className='review-text'>
+                    {review.reviewText}
+                </div>
+                <div className='reviewed-on'>
+                    {date}
+                </div>
+                {sessionUser.id === review.userId && <DeleteReviewModal review={review} />}
             </div>
-            <div className='review-rating'>
-                Rating: {review.rating} <span><i className="fas fa-star review-star"></i></span>
-            </div>
-            <div className='review-text'>
-                {review.reviewText}
-            </div>
-            <div className='reviewed-on'>
-                {date}
-            </div>
-            { sessionUser.id === review.userId && <DeleteReviewModal review={review} />}
-        </div>
-    )
+        )
 }
 
 export default ReviewDetail;
