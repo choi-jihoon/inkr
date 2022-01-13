4ed02633c51871bda370c0c0a7e7df8b171ca0c4
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-01-13 15:46:59 -0800

    styling

--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux';
+
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import MyPortfolioImageDetail from '../MyPortfolioImageDetail';
 import ImageDetail from '../ImageDetail';
 
+import './Search.css'
+
 
 function Search() {
-    const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
     const { searchQuery } = useParams();
 
